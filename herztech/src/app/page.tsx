cf15ca57b6839bc3253c9fb4c817be4d289a930f
herzxxvi <contact.herzmwn@gmail.com>
2024-07-24 09:47:25 +0700

    update package version

--- herztech/src/app/page.tsx
+++ herztech/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6">
       <div className="container">
-        <h1 className="text-4xl font-bold mb-6">YouTube Downloader</h1>
+        <h1 className="text-4xl font-bold mb-6">YouTube Downloader | by HErzTech</h1>
         <form onSubmit={handleDownload}>
           <label htmlFor="video-url" className="block text-sm font-medium text-gray-700 mb-2">
             Enter YouTube Video URL:
